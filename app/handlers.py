68d5c8ff22746bc4bec3adaff3b39f65e8a3e054
ashaposhnikova <polosate@gmail.com>
2016-11-05 14:27:17 +0300

    Add remove subscription button

--- app/handlers.py
+++ app/handlers.py
@@ -1,34 +1,53 @@
-from flask import Flask, abort, request, redirect, url_for, make_response, render_template, flash, g, session
-from beeline_api.rest_api import get_subscribes, get_beeline_token
-# from auth.session import set_token, get_session, authorization
+from flask import request, redirect, url_for, render_template, \
+    flash, g, session
+
+from beeline_api.rest_api import get_subscriptions, \
+    get_beeline_token, remove_subscriptions
+from flask_login import login_user, logout_user, current_user, login_required
 from app.forms import LoginForm, RegistrationForm, is_phone_number_exists
 from app import app, db, lm
-from app.models import User, Ctn, get_user
-from flask_login import login_user, logout_user, current_user, login_required
+from app.models import User, get_user
 
 
-def render_dashboard_page(subscriptions_list=None, subscriptions_str = None, errors=None):
-    return render_template('dashboard.html', subscriptions_list=subscriptions_list, \
-        subscriptions_str = subscriptions_str,errors=errors)
+def render_dashboard_page(
+        subscriptions_list=None,
+        subscriptions_str=None,
+        errors=None
+):
+    return render_template(
+        'dashboard.html',
+        subscriptions_list=subscriptions_list,
+        subscriptions_str=subscriptions_str,
+        errors=errors
+    )
+
 
 @app.before_request
 def before_request():
     g.user = current_user
 
+
 @lm.user_loader
 def load_user(user_id):
     return get_user(user_id)
+
 
 @app.route('/')
 @app.route('/index')
 def index():
     return render_template('index.html', user=current_user.login)
 
+
 @app.route('/login', methods=['POST', 'GET'])
 def login():
     if request.method == 'GET':
             form = LoginForm()
-            return render_template('login.html', form=form, title = 'Home', error=request.args.get('error'))
+            return render_template(
+                'login.html',
+                form=form,
+                title='Home',
+                error=request.args.get('error')
+            )
 
     if g.user is not None and g.user.is_authenticated:
         return redirect(url_for('dashboard'))
@@ -38,16 +57,20 @@ def login():
 
     if is_phone_number_exists(form.ctn1.data):
         session['ctn'] = form.ctn1.data
-    
+
     if is_form_valid:
-        # ctn = form.ctn1.data
         login = form.login.data
         password = form.password.data
-        # session['ctn'] = ctn
-        user = User.query.filter_by(login = login).filter_by(password = password).first()
+        query = User.query.filter_by(login=login).filter_by(password=password)
+        user = query.first()
         login_user(user)
         return redirect(url_for('dashboard'))
-    return render_template('login.html', form=form, title = 'Home', error=request.args.get('error'))
+
+    return render_template(
+        'login.html',
+        form=form,
+        title='Home',
+        error=request.args.get('error')
 
 
 @app.route('/registration', methods=['POST', 'GET'])
@@ -59,7 +82,7 @@ def registration():
 
     if request.method == 'GET':
         form = RegistrationForm()
-        return render_template('registration.html', form=form, title = 'Registration', ctn=ctn)
+        return render_template('registration.html', form=form, title='Registration', ctn=ctn)
     
     if g.user is not None and g.user.is_authenticated:
         return redirect(url_for('dashboard'))
@@ -73,32 +96,28 @@ def registration():
         password = form.password.data
         ctn = form.ctn1.data
         
-        user = User(login = login, password = password, ctn = ctn)
+        user = User(login=login, password=password, ctn=ctn)
         db.session.add(user)
         db.session.commit()
 
-    # remember_me = False
-    # if 'remember_me' in session:
-    #     remember_me = session['remember_me']
-    #     session.pop('remember_me', None)
         login_user(user)
         return redirect(request.args.get('next') or url_for('dashboard'))
     return render_template('registration.html', form=form, title = 'Registration')
 
 
-@app.route("/dashboard")
+@app.route("/dashboard", methods=['POST', 'GET'])
 @login_required
 def dashboard(): 
-        # token = request.cookies.get('token')
-        # session = get_session(token)
-        # if not session:
-        #     return redirect(url_for('login', error="Аутентифицируйтесь, блеа!"))
-
         bt, _ = get_beeline_token()
         ctn = session['ctn']
-        print("Отлдака!", bt, ctn)
 
-        subscriptions, errors = get_subscribes(bt, ctn)
+        if request.method == "POST":
+            subscriptionId = request.args.get("subscriptionId")
+            response_message = remove_subscriptions(bt, ctn, subscriptionId)
+            flash(response_message)
+            return redirect(url_for('dashboard'))        
+
+        subscriptions, errors = get_subscriptions(bt, ctn)
 
         if not errors:
             if isinstance(subscriptions, list):
