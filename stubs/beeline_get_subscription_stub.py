b514769405de9a6fdfa3591fb62f6ff6ed1253d9
ashaposhnikova <polosate@gmail.com>
2016-11-15 12:56:42 +0300

    Add stub

---
+++ stubs/beeline_get_subscription_stub.py
@@ -0,0 +1,29 @@
+from flask import Flask, jsonify, request
+from return_json import return_schema
+
+app = Flask(__name__)
+
+@app.route('/get')
+def get():
+    response = return_schema
+
+    return jsonify(**response)
+
+
+@app.route('/remove')
+def decline():
+    sub_id = request.args.get('subscriptionId')
+
+    response = {
+        "meta": {
+            "status": "OK",
+            "code": 20000,
+            "message": None,
+            "id": sub_id
+        }
+    }
+    return jsonify(**response)
+
+
+if __name__ == "__main__":
+    app.run(port=5050, debug=True)
